2c3aa78892895f6de4dac79ac8bd8661ac86b256
Morion <adrian.martinez@udea.edu.co>
2023-10-14 14:01:08 -0500

    Organizadas las barajas

--- Assets/_Game/Scripts/Menu/SeleccionBaraja.cs
+++ Assets/_Game/Scripts/Menu/SeleccionBaraja.cs
@@ -8,9 +8,24 @@ public class SeleccionBaraja : MonoBehaviour
     public ScriptableDeck deck;
     public GameObject baseImagen;
     public Transform tPadreImagenes;
+    public NombreObjeto[] objetos;
+
 
     void Start()
     {
+		if (objetos.Length > 0)
+		{
+            string ba = PlayerPrefs.GetString("mazo", "MUISCAS");
+			for (int i = 0; i < objetos.Length; i++)
+			{
+				if (ba.Equals(objetos[i].nombre))
+				{
+					gameObject.SetActive(false);
+					objetos[i].objeto.SetActive(true);
+					return;
+				}
+			}
+		}
 		for (int i = 0; i < deck.cards.Length; i++)
 		{
             GameObject g = Instantiate(baseImagen, tPadreImagenes);
@@ -22,4 +37,11 @@ public class SeleccionBaraja : MonoBehaviour
 	{
         PlayerPrefs.SetString("mazo", mazo);
 	}
+}
+
+[System.Serializable]
+public class NombreObjeto
+{
+    public string nombre;
+    public GameObject objeto;
 }
